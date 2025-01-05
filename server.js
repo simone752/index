56d3d3db9eb1956fe06929385fd2b8cb42e2c96f
scarbo <58745420+simone752@users.noreply.github.com>
2025-01-05 21:13:24 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const sqlite3 = require("sqlite3").verbose();
 const cors = require("cors");
 
 const app = express();
-const db = new sqlite3.Database("data.db");
+const db = new sqlite3.Database('./database.sqlite');
 
 app.use(bodyParser.json());
 app.use(cors());
